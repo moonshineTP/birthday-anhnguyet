cf8f765d77689e01708997231658a600cd7b0dc7
MoonshineTP <giahung260305@gmail.com>
2025-05-07 18:45:29 +0700

    Add images and wave effects

--- src/js/index.js
+++ src/js/index.js
@@ -20,15 +20,28 @@ thirdCanvas.setAttribute("style","display:none");
 let containerToggleOne = setTimeout(function(){
 	firstSlideContainer.setAttribute("style","display:none");
 	secondSlideContainer.setAttribute("style","display:block");
-},2500);
+},3000);
 
 
 let removeFirstSlide = setTimeout(function(){
 	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
 	secondCanvas.setAttribute("style","display:block");
-},6500);
+},7000);
 
 let removeSecondCanvas = setTimeout(function(){
 	secondCanvas.setAttribute("style","display:none");
 	thirdCanvas.setAttribute("style","display:block")
 },9800)
+
+let audio = document.getElementById("background-audio");
+let audioToggleButton = document.getElementById("audio-toggle");
+
+audioToggleButton.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+
